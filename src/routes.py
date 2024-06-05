2b7d0d35230a305590fc2f9327884e2a364b7e90
Ivan Berestianyi <berestvania@gmail.com>
2024-06-05 14:55:12 +0300

    added routers for registration and login

---
+++ src/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template, make_response
+from flask_restful import Resource
+
+
+users = {
+    "testuser": "testpassword"
+}
+
+
+class Login(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('login.html'), 200, headers)
+
+
+class Register(Resource):
+    def get(self):
+        headers = {'Content-Type': 'text/html'}
+        return make_response(render_template('register.html'), 200, headers)
+
+
+
